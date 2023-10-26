3b1456edc9a404af02bd2b36a00c1073c532bc62
alexWoon <noowxela@gmail.com>
2023-10-26 14:50:12 +0800

    feat: add sqllite

--- index.js
+++ index.js
@@ -1,22 +1,80 @@
-const express = require('express')
+const express = require("express");
+const fs = require("fs");
+const sqlite = require("sql.js");
+const filebuffer = fs.readFileSync("db/usda-nnd.sqlite3");
 
-const app = express()
+const db = new sqlite.Database(filebuffer);
 
-app.get('/', (req, res) => {
-	res.send('Express JS on Vercel')
-})
+const app = express();
 
-app.get('/ping', (req, res) => {
-	res.send('pong 🏓')
-})
+const COLUMNS = [
+  "carbohydrate_g",
+  "protein_g",
+  "fa_sat_g",
+  "fa_mono_g",
+  "fa_poly_g",
+  "kcal",
+  "description",
+];
 
-const port = process.env.PORT || 8080
+app.get("/", (req, res) => {
+  res.send("Express JS on Vercel");
+});
+
+app.get("/ping", (req, res) => {
+  res.send("pong 🏓");
+});
+
+app.get("/api/food", (req, res) => {
+  const param = req.query.q;
+
+  if (!param) {
+    res.json({
+      error: "Missing required parameter `q`",
+    });
+    return;
+  }
+
+  // WARNING: Not for production use! The following statement
+  // is not protected against SQL injections.
+  const r = db.exec(
+    `
+	  select ${COLUMNS.join(", ")} from entries
+	  where description like '%${param}%'
+	  limit 100
+	`
+  );
+
+  if (r[0]) {
+    res.json(
+      r[0].values.map((entry) => {
+        const e = {};
+        COLUMNS.forEach((c, idx) => {
+          // combine fat columns
+          if (c.match(/^fa_/)) {
+            e.fat_g = e.fat_g || 0.0;
+            e.fat_g = (
+              parseFloat(e.fat_g, 10) + parseFloat(entry[idx], 10)
+            ).toFixed(2);
+          } else {
+            e[c] = entry[idx];
+          }
+        });
+        return e;
+      })
+    );
+  } else {
+    res.json([]);
+  }
+});
+
+const port = process.env.PORT || 8080;
 
 app.listen(port, (err, res) => {
-	if (err) {
-		console.log(err)
-		return res.status(500).send(err.message)
-	} else {
-		console.log('[INFO] Server Running on port:', port)
-	}
-})
+  if (err) {
+    console.log(err);
+    return res.status(500).send(err.message);
+  } else {
+    console.log("[INFO] Server Running on port:", port);
+  }
+});
